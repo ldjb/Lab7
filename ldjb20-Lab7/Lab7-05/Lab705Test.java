422a0799ef038f66713345395a380e1878e88e90
Leon Byford <leonbyford@138-38-151-244.eduroam.bath.ac.uk>
2012-11-29 17:46:51 +0000

    [05] overload Triangle constructor

--- ldjb20-Lab7/Lab7-05/Lab705Test.java
+++ ldjb20-Lab7/Lab7-05/Lab705Test.java
@@ -1,6 +1,12 @@
 public class Lab705Test {
 
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
+	
+		Triangle tri = new Triangle();
+		System.out.println(tri.getLenSide1());
+		tri.setLenSide1(2);
+		System.out.println(tri.getLenSide1());
+	
 	}
 
 }
