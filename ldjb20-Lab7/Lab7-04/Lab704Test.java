682af535f9bf3277ddfdc9df13aa5091d77c18d9
Leon Byford <leonbyford@138-38-151-244.eduroam.bath.ac.uk>
2012-11-29 15:28:10 +0000

    [04-COMPLETE] add example usage

--- ldjb20-Lab7/Lab7-04/Lab704Test.java
+++ ldjb20-Lab7/Lab7-04/Lab704Test.java
@@ -1,6 +1,14 @@
 public class Lab704Test {
 
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
+	
+		TicketMachine machine = new TicketMachine(10);
+		machine.insertMoney(1); // sneaky customer
+		for (int i = 0; i < 5 ; i++) {
+			machine.printTicket();
+		}
+		machine.empty();
+		
 	}
 
 }
