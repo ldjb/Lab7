097bea4369d0019484dd25ffc6d5b229ff98e776
Leon Byford <leonbyford@138-38-151-244.eduroam.bath.ac.uk>
2012-11-30 15:04:47 +0000

    [06] comments

--- ldjb20-Lab7/Lab7-06/BankAccount.java
+++ ldjb20-Lab7/Lab7-06/BankAccount.java
@@ -1,5 +1,6 @@
 public class BankAccount {
 
+	// class attributes
 	private int accNum; // account number
 	private String firstName; // customer's first name
 	private String lastName; // customer's last name
@@ -13,30 +14,37 @@ public class BankAccount {
 		balance = 0;
 	}
 	
+	// get account number
 	public int getAccNum() {
 		return accNum;
 	}
 	
+	// get customer's first name
 	public String getFirstName() {
 		return firstName;
 	}
 	
+	// set customer's first name
 	public void setFirstName(String var) {
 		firstName = var;
 	}
 	
+	// get customer's last name
 	public String getLastName() {
 		return lastName;
 	}
 	
+	// set customer's last name
 	public void setLastName(String var) {
 		lastName = var;
 	}
 	
+	// get account balance
 	public double getBalance() {
 		return balance;
 	}
 	
+	// increment balance by value given; return new balance
 	public double modBalance(double var) {
 		return balance += var;
 	}
