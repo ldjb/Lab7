e9c2cddb166f87242688db9dfd38bf9cffaea6f5
Leon Byford <leonbyford@138-38-151-244.eduroam.bath.ac.uk>
2012-11-30 15:22:02 +0000

    [06-COMPLETE] example

--- ldjb20-Lab7/Lab7-06/Lab706Test.java
+++ ldjb20-Lab7/Lab7-06/Lab706Test.java
@@ -1,6 +1,31 @@
 public class Lab706Test {
-
-	public static void main(String[] args) {	
+	
+	public static void greetUser(BankAccount acc) {
+		System.out.println("Hello " + acc.getFirstName() + ".");
+		System.out.println("Your account number is: " + acc.getAccNum());
+		System.out.println("You have £" + Double.toString(acc.getBalance()) + " in your account.");
 	}
-
+	
+	public static void main(String[] args) {
+		// create account
+		BankAccount acc = new BankAccount(23, "John", "Smith");
+		
+		// add £100 to account
+		acc.modBalance(100);
+		
+		// greet user
+		greetUser(acc);
+		
+		// withdraw £29.99
+		acc.modBalance(-29.99);
+		
+		// change name
+		acc.setFirstName("Philip");
+		acc.setLastName("Pirrip");
+		
+		// greet user again
+		greetUser(acc);
+		
+	}
+	
 }
