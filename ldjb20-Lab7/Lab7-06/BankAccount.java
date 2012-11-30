1883b3d5d99de04a04eb3a0e7e7bbdee0ef5231a
Leon Byford <leonbyford@138-38-151-244.eduroam.bath.ac.uk>
2012-11-30 14:26:37 +0000

    [06] create attributes and constructor

--- ldjb20-Lab7/Lab7-06/BankAccount.java
+++ ldjb20-Lab7/Lab7-06/BankAccount.java
@@ -1,3 +1,16 @@
 public class BankAccount {
 
+	private int accNum; // account number
+	private String firstName; // customer's first name
+	private String lastName; // customer's last name
+	private double balance; // account balance
+	
+	// constructor
+	public BankAccount(int var1, String var2, String var3) {
+		accNum = var1;
+		firstName = var2;
+		lastName = var3;
+		balance = 0;
+	}
+
 }
