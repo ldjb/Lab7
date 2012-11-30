fc99e9d543cf246c24b5a30f5c24b922a425beae
Leon Byford <leonbyford@138-38-151-244.eduroam.bath.ac.uk>
2012-11-30 01:15:40 +0000

    [05-COMPLETE] set up test

--- ldjb20-Lab7/Lab7-05/Lab705Test.java
+++ ldjb20-Lab7/Lab7-05/Lab705Test.java
@@ -2,10 +2,11 @@ public class Lab705Test {
 
 	public static void main(String[] args) {
 	
-		Triangle tri = new Triangle();
-		System.out.println(tri.getLenSide1());
-		tri.setLenSide1(-5);
-		System.out.println(tri.getLenSide1());
+		Triangle tri = new Triangle(10, 11.3, 13.1);
+		System.out.print("Area: ");
+		System.out.println(tri.area());
+		System.out.print("Perimeter: ");
+		System.out.println(tri.perimeter());
 	
 	}
 
